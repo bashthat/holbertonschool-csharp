356eb8b9ad4c1a0c8bfa5d52a9cfc13a30a217d6
bashthat <3249@holbertonschool.com>
2022-08-31 16:54:48 -0500

    editing

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# creating 1-new_project
-dotnet new console -o 1-new_project
-dotnet build 1-new_project
--- 0x00-csharp-hello_world/2-run_new_project.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# creating the new project
-dotnet new project -o 2-new_project
-dotnet build 2-new_project
-dotnet run --project 2-new_project
