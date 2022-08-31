af761ce276d53b438a207acd54449db0c5ade44d
Sam <97474996+bashthat@users.noreply.github.com>
2022-08-31 16:58:03 -0500

    Delete stepone.sh

--- stepone.sh
+++
@@ -1,5 +0,0 @@
-mkdir ~/src && cd ~/src
-wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
