ba2372023947a9d85401bec130000c95ea30da61
bashthat <3249@holbertonschool.com>
2022-08-31 16:51:35 -0500

    downloads for WSL vs POP os

---
+++ dotnet.sh
@@ -0,0 +1,2 @@
+sudo apt-get install -y dotnet-sdk-6.0
+sudo apt-get install -y aspnetcore-runtime-6.0
---
+++ steptwo.sh
@@ -0,0 +1,3 @@
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get update
