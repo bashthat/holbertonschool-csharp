ae1e6d21d1ea0357fbe2defa3ca1fdb5af384101
bashthat <3249@holbertonschool.com>
2022-09-01 09:57:40 -0500

    writeline

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,12 +1,11 @@
 using System;
-
-
-namespace writeline
+ 
+namespace _3_writeline
 {
 	class Program
 	{
 		static void Main(string[] args)
-				{
+		{
 			Console.WriteLine("\"Programming is like building a multilingual puzzle");
 	
 		}
