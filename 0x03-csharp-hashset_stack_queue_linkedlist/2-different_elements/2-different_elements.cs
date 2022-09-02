3d6f50510c4212c32adaa7769d314e294afb6fb1
bashthat <3249@holbertonstudents.com>
2022-09-01 19:49:19 -0700

    comments

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-
+// class List with more Hashing
 class List
 {
 	public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
+	// hashthat
         List<int> alienz = new List<int>();
         foreach (int xyz in list1)
         {
@@ -16,6 +17,7 @@ class List
             if (!list1.Contains(xyz))
                 alienz.Add(xyz);
         }
+	// sort
         alienz.Sort();
         return alienz;
     }
