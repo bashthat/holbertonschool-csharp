037cb26f4c3e852c0484a7000ea474c458e1edc0
bashthat <3249@holbertonstudents.com>
2022-09-01 19:37:29 -0700

    waters

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> dynamic =  new List<int>();
+        foreach (int xyz in list1)
+        {
+            if (list2.Contains(xyz))
+                dynamic.Add(xyz);
+        }
+        return dynamic;
+    }
+}
