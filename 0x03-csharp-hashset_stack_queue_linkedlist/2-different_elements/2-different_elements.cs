69b49bf0741fb09c23a7722ec7111cfac6ef7fd5
bashthat <3249@holbertonstudents.com>
2022-09-01 19:46:44 -0700

    editing

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> alienz = new List<int>();
+        foreach (int xyz in list1)
+        {
+            if (!list2.Contains(xyz))
+                alienz.Add(xyz);
+        }
+        foreach (int xyz in list2)
+        {
+            if (!list1.Contains(xyz))
+                alienz.Add(xyz);
+        }
+        alienz.Sort();
+        return alienz;
+    }
+}
