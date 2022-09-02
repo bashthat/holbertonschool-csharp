58b610bc342d1259b30445d111b3d9ca9db8c370
bashthat <3249@holbertonstudents.com>
2022-09-01 18:12:23 -0700

    files for miles

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    // Main Method
+    public static int Sum(List<int> myList) {
+    }
+        HashSet<int> mySet = new HashSet<int>();
+        foreach (int y in myList)
+        {
+            mySet.Add(y);
+        }
+        int sum = 0;
+        foreach (int y in mySet)
+        {
+            sum += y;
+        }
+        return sum;
+    }
