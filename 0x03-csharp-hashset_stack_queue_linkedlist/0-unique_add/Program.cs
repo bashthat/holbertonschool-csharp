61f491d4660c213ff335d0b661b46e3876a54e59
bashthat <3249@holbertonschool.com>
2022-09-01 20:09:15 -0500

    codez

--- 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/Program.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    // Main Method
+    public static int Sum(List<int> myList) {
+    }
+        HashSet<int> mySet = new HashSet<int>();
+        foreach (int y in myList)
+        {
+            mySet.Add(y);
+        }
+        int sum = 0;
+        foreach (int y in mySet)
+        {
+            sum += y;
+        }
+        return sum;
+    }
