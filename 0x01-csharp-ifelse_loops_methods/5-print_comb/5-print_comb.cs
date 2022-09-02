cc474273cf33b07baec837a0559205d49d06aca5
bashthat <3249@holbertonschool.com>
2022-09-02 17:59:54 -0500

    D2

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 99; i++)
+        for (int i = 0; i <= 99; i++)
         {
-            Console.Write("{0}, ", i);
+            Console.Write("{0:D2}, ", i);
         }
         Console.WriteLine("99");
     }
