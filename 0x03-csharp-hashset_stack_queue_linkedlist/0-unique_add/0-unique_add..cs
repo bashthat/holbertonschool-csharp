84cc3e43fe2baef1aea1ac26d4ac3d4ec8b4923f
bashthat <3249@holbertonschool.com>
2022-09-01 20:54:52 -0500

    hashtags

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add..cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    // Main Method
+    public static int Sum(List<int> myList) {
+        var sum = 0;
+        var unique = new HashSet<int>();
+
+        foreach (var item in myList) {
+            if (unique.Add(item)) {
+                sum += item;
+            }
+        }
+        return sum;
+    }
+}
