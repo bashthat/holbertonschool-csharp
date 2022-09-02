4daeb1e954f573c81aed15f19b852303574e1c04
bashthat <3249@holbertonstudents.com>
2022-09-01 20:44:12 -0700

    dynamix

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+// MyQueue
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        bool dynamic = aQueue.Contains(search); // bool dynamics
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, dynamic);
+        if (dynamic) // if the search is found
+        {
+            string xyz;
+            do {
+                xyz = aQueue.Dequeue();
+            }
+            while (xyz != search);
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+     }
+}
