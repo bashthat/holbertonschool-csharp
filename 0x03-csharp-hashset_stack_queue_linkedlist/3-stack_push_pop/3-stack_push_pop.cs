554f52612882166e42d67949424223eefdedc64f
bashthat <3249@holbertonstudents.com>
2022-09-01 20:00:44 -0700

    word

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+// class declaration
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+	// prototype and arguments
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        else // true? falsE? else statement.
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        // if successful to end the search
+	bool trump = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, trump );
+        if (trump)
+        {
+            string xyz;
+            do {
+                xyz = aStack.Pop();
+            }
+            while (xyz != search);
+        
+}
+        
+	aStack.Push(newItem);
+        return aStack; // word
+    }
+}
