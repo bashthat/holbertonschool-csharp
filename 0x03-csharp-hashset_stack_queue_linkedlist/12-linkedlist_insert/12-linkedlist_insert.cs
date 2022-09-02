d644cdcd4091a9c082195cab5cd6057ce5daf673
bashthat <3249@holbertonschool.com>
2022-09-02 17:09:15 -0500

    edits

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+class LList {
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> lint = myLList.First;
+        while(lint != null)
+        // null
+        {
+            if(lint.Value < n)
+                lint = lint.Next;
+            else
+            // return lint
+            {
+                myLList.AddBefore(lint, n);
+                return myLList.First;
+            }   
+        }
+        // return myLList.First;
+        
+        myLList.AddLast(n);
+        return myLList.First;  
+    } // word
+}
