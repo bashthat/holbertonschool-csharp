0af9f0fa40f30abf5eda60eab5cafed41e0b9491
bashthat <3249@holbertonschool.com>
2022-09-02 22:56:53 -0500

    edits

--- 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -5,7 +5,7 @@ class Dictionary
 {
     public static void PrintSorted(Dictionary<string, string> myDict)
     {
-        var sortedDict = from entry in myDict orderby entry.Key select entry;
+        var sortedDict = myDict.OrderBy(x => x.Key);
         foreach (KeyValuePair<string, string> entry in sortedDict)
         {
             Console.WriteLine("{0}: {1}", entry.Key, entry.Value);
