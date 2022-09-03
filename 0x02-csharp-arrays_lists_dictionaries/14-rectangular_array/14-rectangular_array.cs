9737a4e0cf8e9faab3b917cba4cd473010e78d15
bashthat <3249@holbertonschool.com>
2022-09-02 23:23:22 -0500

    mo

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,22 +1,31 @@
 ﻿using System.Collections.Generic;
 using System;
-
+// class Dictionary<string, string>
 namespace _14_rectangular_array
-{
+{ 
     class Program
-    {
+    // class 
+    { 
         static void Main(string[] args)
         {
-            int[,] myArray = new int[3, 3] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-            Console.WriteLine(myArray[0, 0]);
-            Console.WriteLine(myArray[0, 1]);
-            Console.WriteLine(myArray[0, 2]);
-            Console.WriteLine(myArray[1, 0]);
-            Console.WriteLine(myArray[1, 1]);
-            Console.WriteLine(myArray[1, 2]);
-            Console.WriteLine(myArray[2, 0]);
-            Console.WriteLine(myArray[2, 1]);
-            Console.WriteLine(myArray[2, 2]);
+            int[,] myArray = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    myArray[i, j] = i * j;
+                }
+            } // for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                { // 0 0 0 0 0
+                    Console.Write(myArray[i, j]);
+                    if (j != 4)
+                        Console.Write(" ");
+                } // end for j
+                Console.WriteLine();
+            }
         }
     }
 }
