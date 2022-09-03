d853199486ed36863e6eb75a17f785f7179ba788
bashthat <3249@holbertonschool.com>
2022-09-02 21:39:09 -0500

    test

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return "None";
+        int max = 0;
+        string key = "";
+        foreach (KeyValuePair<string, int> entry in myList)
+        {
+            if (entry.Value > max)
+            {
+                max = entry.Value;
+                key = entry.Key;
+            }
+        }
+        return key;
+    }
+}
--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[,] newMatrix = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+        for (int i = 0; i < myMatrix.GetLength(0); i++) {
+            for (int j = 0; j < myMatrix.GetLength(1); j++) {
+                newMatrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+            }
+        }
+        return newMatrix;
+    }
+}
