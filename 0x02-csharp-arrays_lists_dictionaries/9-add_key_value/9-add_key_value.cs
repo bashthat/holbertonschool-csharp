017577fd3bfc16b3a78c25e6fb5a77a57c7e987c
bashthat <3249@holbertonschool.com>
2022-09-02 23:11:43 -0500

    nine

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
 using System;
-
+// class Dictionary<string, string>
 class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value) {
-        myDict.Add(key, value);
-        return myDict;
+       if (dsx.ContainsKey(key))
+       // the key
+       {
+           dsx[key] = value;
+       }
+       // the value
+       else
+       {
+           dsx.Add(key, value);
+       }
     }
 }
