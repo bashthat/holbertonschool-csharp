2d03123be8c5ce2dbf15a92380f1c37014fa8c73
bashthat <3249@holbertonschool.com>
2022-09-02 23:17:08 -0500

    nine

--- 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -6,7 +6,7 @@ class Dictionary
 {
     public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value) {
         myDict.Add(key, value);
-        myDict.TryGetValue(key, out value);
+        // myDict.TryGetValue(key, out value);
         myDict.Remove(key);
         return myDict;
     }
