7b21a02d642c37dcd7f43e378e31bc4795eb9f8b
bashthat <3249@holbertonschool.com>
2022-09-06 15:04:02 -0500

    integer defined!

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -3,13 +3,13 @@ using System;
 
 class Int {
     public static void divide(int a, int b) {
+        xyz = 0;
         try {
-            Console.WriteLine(a / b);
+            xyz = a / b;
         } catch (DivideByZeroException) {
             Console.WriteLine("Cannot divide by zero");
-        }
-        finally {
-            Console.WriteLine("Inside divide");
+        } finally {
+            Console.WriteLine("{0} / {1} = {2}", a, b, xyz);
         }
     }
 }
