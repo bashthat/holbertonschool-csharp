eb7fcaf054b5e30dfaf221482082aa912dc13450
Sam <97474996+bashthat@users.noreply.github.com>
2022-09-06 15:52:55 -0500

    re-try

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -1,16 +1,33 @@
 using System;
 
-// class Int
+// class Int create an integer
 
-class Int {
-  public static void divide(int a, int b) {
-    // to print int a or int b given divisibility
-    
-    try {
-      c = (a / b);
-    } catch {
-      Console.WriteLine("Cannot divide by zero");
-    } finally {
-      Console.WriteLine("{0} / {1} = {2}", a, b, c);
+class Int 
+{
+    public static void divide(int a, int b) 
+    // try to divide a by b
+    // if b is 0, print Cannot divide by zero
+    // if b is not an integer, print Cannot divide by non-number
+    // if a or b is less than 0, print Cannot divide by negative number
+    // finally, print a / b = result
+    {
+        try
+        {
+            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Cannot divide by non-number");
+        }
+        finally
+        {
+            if (a < 0 || b < 0)
+            {
+                Console.WriteLine("Cannot divide by negative number");
+            }
+        }
     }
-  } 
