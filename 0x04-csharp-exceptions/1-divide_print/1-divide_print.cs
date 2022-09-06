30e626d8ea470ec19c4be40e0238c83f3d74b544
Sam <97474996+bashthat@users.noreply.github.com>
2022-09-06 16:00:49 -0500

    simplify

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -5,29 +5,20 @@ using System;
 class Int 
 {
     public static void divide(int a, int b) 
-    // try to divide a by b
-    // if b is 0, print Cannot divide by zero
-    // if b is not an integer, print Cannot divide by non-number
-    // if a or b is less than 0, print Cannot divide by negative number
-    // finally, print a / b = result
+    
+        // try to divide a by b
+        // if a or b are equal to zero then print "Cannot divide by zero"
+    
     {
         try
         {
-            Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
+            Console.WriteLine(a / b);
         }
         catch (DivideByZeroException)
         {
             Console.WriteLine("Cannot divide by zero");
         }
-        catch (FormatException)
-        {
-            Console.WriteLine("Cannot divide by non-number");
-        }
         finally
         {
-            if (a < 0 || b < 0)
-            {
-                Console.WriteLine("Cannot divide by negative number");
-            }
         }
     }
