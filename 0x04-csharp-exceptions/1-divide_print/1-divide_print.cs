9e3fb9b05ed8436ba620a20d8e03afe885def7f3
Sam <97474996+bashthat@users.noreply.github.com>
2022-09-06 15:44:02 -0500

    formatting sytax

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -2,22 +2,15 @@ using System;
 
 // class Int
 
-class Int 
-{
-    public static void divide(int a, int b)
-    {
-        // to print int a or int b given divisibility
-        int print = 0;
-        try
-        {
-            print = ( a / b );
-        }
-        catch
-        {
-            Console.WriteLine("Cannot divide by zero");
-        }
-        finally
-        {
-            Console.WriteLine("{0} / {1} = {2}", a, b, print);
-        }
-    } // word
+class Int {
+  public static void divide(int a, int b) {
+    // to print int a or int b given divisibility
+    int c = 0;
+    try {
+      print = (a / b);
+    } catch {
+      Console.WriteLine("Cannot divide by zero");
+    } finally {
+      Console.WriteLine("{0} / {1} = {2}", a, b, c);
+    }
+  } 
