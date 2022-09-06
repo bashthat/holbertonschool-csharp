042bfcb5a4c1b5bb78751bc65f48e0e44b158ab8
Sam <97474996+bashthat@users.noreply.github.com>
2022-09-06 16:22:24 -0500

    reformat

--- 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -2,27 +2,21 @@ using System;
 
 // class Int create an integer
 
-class Int 
-{
-    public static void divide(int a, int b) 
-    
-        // try to divide a by b
-        // if a or b are equal to zero then print "Cannot divide by zero"
-    {
-        int xyz = 0;
-        // more literal syntax
-        try
-        {
-            xyz = a / b;
-        }
-        catch (System.DivideByZeroException)
-        {
-            Console.WriteLine("Cannot divide by zero");
-        }
-        finally
-        {
-            // Console.WriteLine("{0} / {1} = {2}", a, b, xyz);
-            Console.WriteLine($"{a} / {b} = {xyz}");
-        }
+class Int {
+  public static void divide(int a, int b)
+
+  // try to divide a by b
+  // if a or b are equal to zero then print "Cannot divide by zero"
+  {
+    int xyz = 0;
+    // more literal syntax
+    try {
+      xyz = a / b;
+    } catch (System.DivideByZeroException) {
+      Console.WriteLine("Cannot divide by zero");
+    } finally {
+      // Console.WriteLine("{0} / {1} = {2}", a, b, xyz);
+      Console.WriteLine($"{a} / {b} = {xyz}");
     }
+  }
 } // wroks
