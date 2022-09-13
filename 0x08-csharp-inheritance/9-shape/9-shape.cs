2b383806904891681bfff1bf925f6701b3bf4b8f
bashthat <3249@holbertonschool.com>
2022-09-13 16:58:37 -0500

    nine

--- 0x08-csharp-inheritance/9-shape/9-shape.cs
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -1,2 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Rectangles;
+
+/// <summary>Initializes Class Obj</summary>
+class Shape
+{
+    /// <summary>Initializes Class Obj</summary>
+    /// <returns>???nothing!!!</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>Initializes Class Obj</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else if (value == 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+    /// <summary>Initializes Class Obj</summary>
+    /// <returns>???nothing!!!</returns>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else if (value == 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+    /// <summary>Initializes Class Obj</summary>
+    /// <returns>???nothing!!!</returns>
+    public new int Area()
+    {
+        return width * height;
+    }
+    /// <summary>Initializes Class Obj</summary>
+    /// <returns>???nothing!!!</returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+/// <summary>Initializes Class Obj</summary>
+/// <returns>???nothing!!!</returns>
+
+class Square : Rectangle
+{
+    private int size;
+    public int Size
+    {
+        get { return size; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            else if (value == 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            else
+                size = value;
+        }
+    }
+    /// <summary>Initializes Class Obj</summary>
+    /// <returns>???nothing!!!</returns>
+    public new int Area()
+    {
+        return size * size;
+    }
+    /// <summary>Initializes Class Obj</summary>
+    /// <returns>???nothing!!!</returns>
+    public override string ToString()
+    {
+        return $"[Square] {size} / {size}";
+    }
+}
