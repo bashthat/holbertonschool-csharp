91eb277de46b1b27c80054865be736e69bfc5c56
bashthat <3249@holbertonschool.com>
2022-09-12 23:01:32 -0500

    dotnet new classlib

--- 0x07-csharp-tdd/0-add/MyMath/Program.cs
+++ 0x07-csharp-tdd/0-add/MyMath/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+    {
+    /// <summary>Operations class</summary>
+    
+        public class Operations
+        {
+            /// <summary>Adds two integers</summary>
+            /// <param name="a">First integer</param>
+            /// <param name="b">Second integer</param>
+            /// <returns> sum of a and b </returns>
+            public static int Add(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
