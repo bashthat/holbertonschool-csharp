473992af11f825fab72c27686772fd59e173faf1
bashthat <3249@holbertonschool.com>
2022-09-12 23:26:55 -0500

    editing

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NUnit.Framework;
+// using MyMath;
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var coffee = MyMath.Operations.Add(-1, -2);
+            Assert.AreEqual(-3, coffee);
+        }
+        [Test(-40, 200)]
+        public void Test2(int a, int b)
+        {
+            var coffee = Operations.Add(a, b);
+            Assert.AreEqual(160, coffee);
+        }
+        [Test]
+        public void Test3()
+        {
+            var coffee = Operations.Add(3, 7);
+            Assert.AreEqual(10, coffee);
+        }
+    }
+}
