795223943b75ce4a39de61260b442643a105dd35
bashthat <3249@holbertonschool.com>
2022-09-19 23:53:32 -0500

    system

--- 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
+using System.Numerics;
+using System;
 using System.Collections.Generic;
 
 /// <summary>Initializes Class Obj</summary>
@@ -7,16 +9,22 @@ class MatrixMath
     /// <summary>Method that transposes a matrix and returns the resulting matrix</summary>
     public static double[,] Transpose(double[,] matrix)
     {
-        double[,] transposedMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+        /// <summary>Transposes the matrix</summary>
+        double[,] transposedMatrix = {};
 
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrix.GetLength(1); j++)
+        if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            return transposedMatrix;
 
+            transposedMatrix[0] = new double[matrix.GetLength(0), matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                transposedMatrix[j, i] = matrix[i, j];
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    transposedMatrix[i, j] = matrix[j, i];
+                }
             }
-        }
-        return transposedMatrix;
+            /// <summary> returns the matrix transposed</summary>
+            
+            return transposedMatrix;
     }
-}
