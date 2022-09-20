e3dae4dbee9255ef4d22fe6c96e2d0fe3689749c
bashthat <3249@holbertonschool.com>
2022-09-19 23:35:58 -0500

    gitit

--- 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -7,15 +7,16 @@ class MatrixMath
     /// <summary>Method that transposes a matrix and returns the resulting matrix</summary>
     public static double[,] Transpose(double[,] matrix)
     {
-        int input_1 = matrix.GetLength(0);
-        int input_2 = matrix.GetLength(1);
-        double[,] output = new double[input_2, input_1];
+        double[,] transposedMatrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
 
-        for (int i = 0; i < input_1; i++)
-            for (int j = 0; j < input_2; j++)
-                output[j, i] = matrix[i, j];
-        
-        return output;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
 
+            {
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+        return transposedMatrix;
     }
 }
