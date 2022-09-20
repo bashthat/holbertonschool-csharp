5e23e78fb5f5c55afbb79fe2051a296ba5c3f53b
bashthat <3249@holbertonschool.com>
2022-09-19 23:25:52 -0500

    pancere

--- 0x00-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x00-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -8,29 +8,26 @@ class MatrixMath
     /// method to shear
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        double[] result = new double[matrix.Length, direction, factor];
-        double[,] output = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            double[,] output = new double[2, 2];
+            double[,] shear = {{-1}};
+            double[,] output2;
 
-        if (matrix.GetLength(0) != matrix.GetLength(1))
-            return new double[,] {{-1}};
-
-        if (direction == 'x')
-        {
-            output[0, 0] = matrix[0, 0] + (matrix[0, 1] * factor);
-            output[0, 1] = matrix[0, 1];
-            output[1, 0] = matrix[1, 0] + (matrix[1, 1] * factor);
-            output[1, 1] = matrix[1, 1];
-        }
-        else if (direction == 'y')
-        {
-            output[0, 0] = matrix[0, 0];
-            output[0, 1] = matrix[0, 1] + (matrix[0, 0] * factor);
-            output[1, 0] = matrix[1, 0];
-            output[1, 1] = matrix[1, 1] + (matrix[1, 0] * factor);
-        }
-        else
-            return new double[,] {{-1}};
-
-        return output;
+            if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+                return shear;
+            if (direction == 'x')
+            {
+                shear = new double[,] {{1, factor}, {0, 1}};
+                output2 = MatrixMath.Multiply(matrix, shear);
+                return output2;
+            }
+            else if (direction == 'y')
+            {
+                shear = new double[,] {{1, 0}, {factor, 1}};
+                output2 = MatrixMath.Multiply(matrix, shear);
+                return output2;
+            }
+            else
+                return shear;
+            
     }
 }
