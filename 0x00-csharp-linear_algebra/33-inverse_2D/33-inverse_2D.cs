33821184e7c9f993a6ffabeca187648f6629d114
bashthat <3249@holbertonschool.com>
2022-09-19 23:07:03 -0500

    last call

--- 0x00-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x00-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+/// <summary> class </summary>
+class MatrixMath
+{
+    ///<summary> method </summary>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        double[,] result = new double[2, 2];
+        double det = MatrixMath.Determinant(matrix);
+        if (det == 0 || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+            return new double[,] {{-1}};
+        result[0, 0] = Math.Round(matrix[1, 1] / det, 2);
+        result[0, 1] = Math.Round(-1 * matrix[0, 1] / det, 2);
+        result[1, 0] = Math.Round(-1 * matrix[1, 0] / det, 2);
+        result[1, 1] = Math.Round(matrix[0, 0] / det, 2);
+        return result;
+    }
+}
