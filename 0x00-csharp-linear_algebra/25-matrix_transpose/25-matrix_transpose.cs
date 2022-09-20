6654f731eea209f8a16e717479aa47e28b469afa
bashthat <3249@holbertonschool.com>
2022-09-19 23:00:28 -0500

    filez

--- 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x00-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.SymbolStore;
+using System;
+using System.Collections.Generic;
+
+/// <summary>Initializes Class Obj</summary>
+class MatrixMath
+{
+    /// <summary>Method that transposes a matrix and returns the resulting matrix</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] result = new double[matrix.GetLength(1), matrix.GetLength(0)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result[j, i] = matrix[i, j];
+            }
+        }
+        return result;
+    }
+}
--- 0x00-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x00-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+/// <summary> class </summary>
+class MatrixMath
+
+{
+    ///<summary> method </summary>
+    public static double Determinant(double[,] matrix)
+    {
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
+        {
+            return Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+        }
+        else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
+        {
+            double[,] temp = new double[2, 2];
+            double sum = 0;
+            double det = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                int tempI = 0;
+                for (int j = 1; j < 3; j++)
+                {
+                    int tempJ = 0;
+                    for (int k = 0; k < 3; k++)
+                    {
+                        if (k == i)
+                            continue;
+                        temp[tempI, tempJ] = matrix[j, k];
+                        tempJ++;
+                    }
+                    tempI++;
+                }
+                sum += matrix[0, i] * Determinant(temp);
+                if (i % 2 == 1)
+                    det -= sum;
+                else
+                    det += sum;
+                sum = 0;
+            }
+            return Math.Round(det, 2);
+        }
+        else
+            return -1;
