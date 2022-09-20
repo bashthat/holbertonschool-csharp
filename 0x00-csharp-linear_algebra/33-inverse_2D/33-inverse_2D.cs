5424367a3db586ab500409af53ede515c05c5c19
bashthat <3249@holbertonschool.com>
2022-09-19 23:13:06 -0500

    editing

--- 0x00-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
+++ 0x00-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -6,14 +6,22 @@ class MatrixMath
     ///<summary> method </summary>
     public static double[,] Inverse2D(double[,] matrix)
     {
-        double[,] result = new double[2, 2];
-        double det = MatrixMath.Determinant(matrix);
-        if (det == 0 || matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        int input_1 = matrix.GetLength(0);
+        int input_2 = matrix.GetLength(1);
+        double[,] output = new double[input_1, input_2];
+
+        if (input_1 != 2 || input_2 != 2)
             return new double[,] {{-1}};
-        result[0, 0] = Math.Round(matrix[1, 1] / det, 2);
-        result[0, 1] = Math.Round(-1 * matrix[0, 1] / det, 2);
-        result[1, 0] = Math.Round(-1 * matrix[1, 0] / det, 2);
-        result[1, 1] = Math.Round(matrix[0, 0] / det, 2);
-        return result;
+
+        double det = Math.Round((matrix[0, 0] * matrix[1, 1]) - (matrix[0, 1] * matrix[1, 0]), 2);
+        if (det == 0)
+            return new double[,] {{-1}};
+
+        output[0, 0] = Math.Round(matrix[1, 1] / det, 2);
+        output[0, 1] = Math.Round(-1 * matrix[0, 1] / det, 2);
+        output[1, 0] = Math.Round(-1 * matrix[1, 0] / det, 2);
+        output[1, 1] = Math.Round(matrix[0, 0] / det, 2);
+
+        return output;
     }
 }
