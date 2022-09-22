d91dcbf436a0b58c9d7558b6f52fb5c47b236592
bashthat <3249@holbertonstudents.com>
2022-09-21 18:41:26 -0700

    editing

--- 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,59 +1,51 @@
 ﻿using System;
 
-/// <summary>Base an abstract class</summary>
-public abstract class Base {
-	/// <summary>Property name</summary>
-	public string name { get; set; }
+abstract class Base
+{
+    /// <summary>
+    /// base class
+    /// </summary>
+    public string name = "" ;
+    public override ToString()
+    {
+    return (this.name + " is a " + this.GetType().Name);
+    }
+}
 
-	/// <summary>Override ToString method</summary>
-	public override string ToString() {
-		return (this.name + "is a" this.GetType().ToString());
-	}
+interface IInteractive
+{
+    void Interact();
 }
-/// <summary>Interface IInteractive</summary>
-public interface IInteractive {
-	/// <summary>Method Interact</summary>
-	void Interact();
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
 }
-/// <summary>Interface IBreakable</summary>
-public interface IBreakable {
-	/// <summary>Property durability</summary>
-	int durability {
-		get;
-		set;
-	}
-	/// <summary>Method Break</summary>
-	void Break();
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
 }
-/// <summary>Interface ICollectable</summary>
-public interface ICollectable {
-	/// <summary>Property isCollected</summary>
-	bool isCollected {
-		get;
-		set;
-	}
-	/// <summary>Method Collect</summary>
-	void Collect();
-}
-/// <summary>Class TestObject</summary>
-public class TestObject: Base,
-IInteractive,
-IBreakable,
-ICollectable {
-	/// <summary>Property durability</summary>
-	public int durability {
-		get;
-		set;
-	}
-	/// <summary>Property isCollected</summary>
-	public bool isCollected {
-		get;
-		set;
-	}
-	/// <summary>Method Interact</summary>
-	public void Interact() {}
-	/// <summary>Method Break</summary>
-	public void Break() {}
-	/// <summary>Method Collect</summary>
-	public void Collect() {}
+/// <summary>
+/// test class
+/// </summary>
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public bool isCollected { get; set; }
+    public int durability { get; set; }
+
+    public void Break()
+    {
+
+    }
+    public void Interact()
+    {
+
+    }
+    public void Collect()
+    {
+
+    }
 }
