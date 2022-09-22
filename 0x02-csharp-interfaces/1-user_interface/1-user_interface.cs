f9cfcc33c4bf83febe87af6ee6d5fb8e03ce803e
bashthat <3249@holbertonschool.com>
2022-09-21 21:00:25 -0500

    stix

--- 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -5,15 +5,15 @@
 abstract class Base
 {
     
-    public string name = { get; set; };
-
+    public string name = { get; set; }; 
     
     public override string ToString()
     {
-        return $"{name} is a {this.GetType()}";
+        return (name + " is a " + this.GetType());
     }
    
 }
+
 interface IInteractive
 {
     void Interact();
