5ac66bcd72106bdcb9de2b1f60c63adcaaa6e048
bashthat <3249@holbertonschool.com>
2022-09-21 21:12:20 -0500

    class do0or

--- 0x02-csharp-interfaces/2-doors/2-doors.cs
+++ 0x02-csharp-interfaces/2-doors/2-doors.cs
@@ -4,46 +4,55 @@
 public abstract class Base
 {
     /// <summary>Property name</summary>
-    public string name { get; set; }
+    public string name 
+    {
+        get;
+        set;
+    }
 
     /// <summary>Override ToString method</summary>
     public override string ToString()
     {
         return $"{name} is a {this.GetType()}";
     }
-    /// <summary>Interface IInteractive</summary>
-    public interface IInteractive
+}
+
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+/// <summary>Door class</summary>
+
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
     {
-        /// <summary>Method Interact</summary>
-        void Interact();
+        this.name = name;
     }
-    /// <summary>Interface IBreakable</summary>
-    public interface IBreakable
+
+    public void Interact()
     {
-        /// <summary>Property durability</summary>
-        int durability { get; set; }
-        /// <summary>Method Break</summary>
-        void Break();
+        Console.WriteLine($"You try to open the {name}. It's locked.");
     }
-    /// <summary>Interface ICollectable</summary>
-    public interface ICollectable
+
+    public void Break()
     {
-        /// <summary>Property isCollected</summary>
-        bool isCollected { get; set; }
-        /// <summary>Method Collect</summary>
-        void Collect();
     }
-    /// <summary>Class Door</summary>
-    public class Door : Base, IInteractive
+
+    public void Collect()
     {
-        /// <summary>Constructor</summary>
-        public Door(string name = "Door")
-        {
-            this.name = name;
-        }
-        /// <summary>Method Interact</summary>
-        public void Interact()
-        {
-            Console.WriteLine($"You try to open the {name}. It's locked.");
-        }
     }
+}
