88600a84400e9dac7db45eb4d8d2c21a638f3260
bashthat <3249@holbertonschool.com>
2022-09-21 20:52:56 -0500

    drums

--- 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -1,53 +1,55 @@
 ﻿using System;
 
 /// <summary>Base an abstract class</summary>
-public abstract class Base
+
+abstract class Base
 {
-    /// <summary>Property name</summary>
+    
     public string name = "";
 
-    /// <summary>Override ToString method</summary>
+    
     public override string ToString()
     {
         return $"{name} is a {this.GetType()}";
     }
-    /// <summary> IInteractive </summary>
+   
 }
 interface IInteractive
 {
     void Interact();
 }
-/// <summary>IBreakable</summary>
+
 interface IBreakable
 {
     int durability { get; set; }
     void Break();
 }
-/// <summary>ICollectable</summary>
+
 interface ICollectable
 {
     bool isCollected { get; set; }
     void Collect();
 }
-/// <summary>TestObject</summary>
+/// <summary>Door class</summary>
+
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
     public int durability { get; set; }
     public bool isCollected { get; set; }
 
-/// <summary>Interact</summary>
+
 
     public void Interact()
     {
     }
 
-/// <summary>Break</summary>
+
 
     public void Break()
     {
     }
 
-/// <summary>Collect</summary>
+
 
     public void Collect()
     {
