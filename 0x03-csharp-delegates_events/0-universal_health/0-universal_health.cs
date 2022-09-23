c275adbf2c46822ccf8c8671c300d8030f50dd83
bashthat <3249@holbertonschool.com>
2022-09-22 19:40:23 -0500

    deligate

---
+++ 0x03-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name="Player", float maxHp=100f)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            maxHp = 100f;
+        }
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
+    }
+}
