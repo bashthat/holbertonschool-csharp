16c334de9aa427bc4e52f1b7d1a165078bd0c910
bashthat <3249@holbertonschool.com>
2022-09-22 19:56:58 -0500

    editingxml

--- 0x03-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ 0x03-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,16 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-/// <summary>
-/// Delegate for calculating health
-/// </summary>
-
 
 class Player
 {
     private string name;
     private float maxHp;
     private float hp;
-    
+
     public Player(string name="Player", float maxHp=100f)
     {
         if (maxHp <= 0)
@@ -22,9 +18,19 @@ class Player
         this.maxHp = maxHp;
         this.hp = maxHp;
     }
- 
+
     public void PrintHealth()
     {
         Console.WriteLine($"{this.name} has {this.hp} / {this.maxHp} health");
     }
 }
+    delegate void CalculateHealth(float amount);
+    class CurrentHPArgs : EventArgs
+    {
+        public readonly float currentHp;
+        public CurrentHPArgs(float newHp)
+        {
+            currentHp = newHp;
+        }
+    }
+    
